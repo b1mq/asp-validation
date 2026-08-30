058c89230a40c3644ebeaede87d635419555c930
Egor <arnoldtagirov1@gmail.com>
2026-08-30 13:23:47 +0200

    Created ValidationService + Validation Interface

---
+++ AspNetCore.Validation.StudentsDb/Services/ValidationService.cs
@@ -0,0 +1,17 @@
+﻿using AspNetCore.Validation.StudentsDb.Interfaces;
+using Validation.Models;
+
+namespace AspNetCore.Validation.StudentsDb.Services
+{
+    public interface ValidationService:IValidationService
+    {
+        public void ValidateStudent(Student student, ModelStateDictionary modelState)
+        {
+            if (student.Surname == "admin")
+                modelState.AddModelError("Surname", "admin - заборонене прізвище");
+
+            if (student.Name == student.Email)
+                modelState.AddModelError("", "ім’я та електронна адреса не повинні збігатися");
+        }
+    }
+}
